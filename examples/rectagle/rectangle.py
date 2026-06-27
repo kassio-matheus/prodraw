ae24817a95309273a8c01520df3378681f90aaa6
italo <githubconta292@gmail.com>
2026-06-27 18:57:23 -0300

    Adding the rectangle drawing function

---
+++ examples/rectagle/rectangle.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+
+
+root = Tk()
+
+canvas = Canvas(root, bg='white', width=600, height=600)
+canvas.pack()
+
+
+retangulos  = []
+# Quando o mouse é pressionado
+def inicia_linha(event):
+    global ini_x, ini_y
+    ini_x = event.x
+    ini_y = event.y
+
+# Quando o mouse é movido com o botão pressionado
+def atualiza_linha(event):
+    global fim_x, fim_y
+    fim_x = event.x
+    fim_y = event.y
+    desenhar()
+    canvas.create_rectangle(ini_x, ini_y, fim_x, fim_y)
+
+# Quando o mouse é solto
+def incluir_linha(event):
+    retangulos.append((ini_x, ini_y, fim_x, fim_y))
+
+def desenhar():
+    canvas.delete("all")
+    for linha in retangulos:
+        canvas.create_rectangle(linha[0], linha[1], linha[2], linha[3])
+
+
+ini_x = None
+ini_y = None
+fim_x = None
+fim_y = None
+canvas.bind('<ButtonPress-1>', inicia_linha)
+canvas.bind('<B1-Motion>', atualiza_linha)
+canvas.bind('<ButtonRelease-1>', incluir_linha)
+
+root.mainloop()
