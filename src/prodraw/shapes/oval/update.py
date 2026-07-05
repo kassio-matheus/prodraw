92b1a35d30a1027720494fca667c10712987773a
potinhodatupiwe <githubconta292@gmail.com>
2026-07-05 00:03:21 -0300

    Fix invalid ovals

--- src/prodraw/shapes/oval/update.py
+++ src/prodraw/shapes/oval/update.py
@@ -17,8 +17,8 @@ class Update:
             obj["obj"].end_x = event.x
             obj["obj"].end_y = event.y
             Draw.draw(obj["obj"].canvas, figures)
-
-            obj["obj"].canvas.create_oval(obj["obj"].start_x, obj["obj"].start_y, obj["obj"].end_x, obj["obj"].end_y,
+            if abs(obj["obj"].end_y-obj["obj"].start_y)>5 and abs(obj["obj"].end_x-obj["obj"].start_x)>5:
+                obj["obj"].canvas.create_oval(obj["obj"].start_x, obj["obj"].start_y, obj["obj"].end_x, obj["obj"].end_y,
                                           fill=SHAPE_COLORS.get(obj["obj"].bg), outline=obj['obj'].bg, tags=("oval", "shape"))
 
         return update_points
